b27298725a204007e5b7f495a262363c81e55a29
anvarl <anvaraidarbekov@gmail.com>
2023-07-22 14:27:43 +0600

    commit and push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Boss villain;
+        villain = new Boss(700, 50, new Weapon(WeaponType.HAMMER,"Thunder"));
+
+        System.out.println(villain.printInfo());
+
+        Skeleton skeleton1 = new Skeleton(400, 30,new Weapon(WeaponType.BOW, "Normal "),10);
+        Skeleton skeleton2 = new Skeleton(380, 30, new Weapon(WeaponType.BOW,"Normal "),10);
+        System.out.println(skeleton1.printInfo());
+        System.out.println(skeleton2.printInfo());
     }
-}
+    }
--- src/Skeleton.java
+++ src/Skeleton.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Skeleton {
+public class Skeleton extends Boss {
+
+    private int arrows;
+
+    public int getArrows() {
+        return arrows;
+    }
+    @Override
+    public String printInfo(){
+        return "BOSS: " + " HEALTH: " + Boss.getHealth() + " DAMAGE: " + Boss.getDamage()
+                + " WEAPON: " + Boss.getHammer().getWeapon()
+                + " NAME OF THE WEAPON: " + Boss.getHammer().getNameOfWeapon() + "AMOUNT OF ARROWS "+ getArrows();
+    }
+
+    public void setArrows(int arrows) {
+        this.arrows = arrows;
+    }
+
+    public Skeleton(int health, int damage, Weapon hammer, int arrows) {
+        super(health, damage, hammer);
+        this.arrows = arrows;
+    }
 }
